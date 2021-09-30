20ec2740347f3a897ce8234d1ab15b490f69f1be
Christine Yoo <christinecho.s@gmail.com>
2021-09-30 10:08:19 -0700

    Add nav bar and link

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,19 @@
 //This components folder contains any components that are not pages (routes)
 //Convention: use lowercase for pages and Uppercase for any other components
 //Layout will wrap around any child components (_app.js)
+import Nav from './Nav'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
     return (
+        <>
+        <Nav />
         <div className={styles.container}>
             <main className={styles.main}>
                 {children}
             </main>
         </div>
+        </>
     )
 }
 
